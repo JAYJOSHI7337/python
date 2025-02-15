d15cc87907ac9d3a0279e9e3497a9fc20ec3e6b1
JAY <jayjoshi733@gmail.com>
2025-02-15 21:42:37 +0100

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+'''
+Author: Harry
+Licenced to: ABC Company   
+***thank You for Reading *******8
+'''
+import os
+print("Hello world. it is my first program.")
---
+++ 04_pr_04_list_directory.py
@@ -0,0 +1,6 @@
+# Author: Harry 
+# Location: Mars
+# Date: 19/03/1993
+
+import os
+print(os.listdir())
